a3e35bac69117e592db95f6b9c251234052065a5
jmike2689 <jmike2689@yahoo.com>
2020-01-11 17:31:30 -0600

    Adding Final HW

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        name: "Barney Stinson",
+        //photo: 
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Ted Mosby",
+        //photo:,
+        scores: [
+            "4",
+            "2",
+            "3",
+            "3",
+            "4",
+            "2",
+            "1",
+            "5",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Robin Scherbatsky",
+        //photo:,
+        scores: [
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "5",
+            "4",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Lily Aldrin",
+        //photo:,
+        scores: [
+            "2",
+            "4",
+            "3",
+            "5",
+            "2",
+            "2",
+            "5",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Marshall Ericksen",
+        //photo:,
+        scores: [
+            "3",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
